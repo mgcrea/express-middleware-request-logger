13971d80b6294e49dc232bbb75e249af624355d2
Olivier Louvignes <olivier@mgcrea.io>
2017-11-28 15:35:45 +0100

    chore(git): initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import Promise from 'bluebird';
+import {kebabCase} from 'lodash';
+import rimraf from 'rimraf';
+import mkdirp from 'mkdirp';
+
+Promise.promisifyAll(fs);
+
+export default function requestLoggerMiddleware({dest = './tmp/requests', clear = false} = {}) {
+  // Create dest folder if missing
+  if (!fs.existsSync(dest)) {
+    mkdirp.sync(dest);
+  }
+  // Clear folder if asked to
+  if (clear) {
+    rimraf.sync(path.join(dest, '*'), {}, () => {});
+  }
+  return (req, res, next) => {
+    const date = Date.now();
+    const kebabCasedUrl = kebabCase(req.originalUrl).substr(0, 30);
+    const method = req.method.toLowerCase();
+    const end = res.end;
+    res.end = (...args) => {
+      fs.writeFileAsync(path.join(dest, `req-${method}-${kebabCasedUrl}-${date}-res.json`), args[0]);
+      end.apply(res, args);
+    };
+    next();
+    const headersFilepath = path.join(dest, `req-${method}-${kebabCasedUrl}-${date}-headers.json`);
+    const bodyFilepath = path.join(dest, `req-${method}-${kebabCasedUrl}-${date}-body.json`);
+    return Promise.all([
+      fs.writeFileAsync(headersFilepath, JSON.stringify(req.headers, null, 2)),
+      req._body ? fs.writeFileAsync(bodyFilepath, JSON.stringify(req.body, null, 2)) : Promise.resolve() // eslint-disable-line no-underscore-dangle
+    ]);
+  };
+}
---
+++ test/spec/index.spec.js
@@ -0,0 +1,46 @@
+
+import expect from 'expect';
+import request from 'request-promise';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import requestLogger from './../../src';
+
+// Configuration
+const host = 'http://localhost:3000';
+const uri = `${host}/test`;
+let server;
+
+beforeAll(() => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(requestLogger({dest: `${__dirname}/../.tmp/requests`, clear: true}));
+  app.all('/test', (req, res) => res.json({ok: 1}));
+  return new Promise((resolve, reject) => {
+    server = app.listen(3000, () => {
+      resolve();
+    });
+  });
+});
+
+describe('requestLogger()', () => {
+  it('constructor should export a function', () => {
+    expect(requestLogger).toBeA('function');
+  });
+  it('should handle basic GET requests', () => (
+    request({uri, json: true})
+      .then((res) => {
+        expect(res).toEqual({ok: 1});
+      })
+  ));
+  it('should handle basic POST requests', () => (
+    request({method: 'POST', uri, json: true, body: {foo: 'bar'}})
+      .then((res) => {
+        expect(res).toEqual({ok: 1});
+      })
+  ));
+});
+
+afterAll(() => {
+  server.close();
+});
